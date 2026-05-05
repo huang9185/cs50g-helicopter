49ad10726d388247b4e40ebbb5e341d319654ce2
huang9185 <helyn20012@outlook.com>
2026-05-05 16:39:46 -0400

    Initialize

---
+++ Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(SpawnGems());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator SpawnGems() {
+		while (true) {
+			int gemsThisRow = 1;
+
+			for (int i = 0; i < gemsThisRow; i++) {
+				Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+			}
+
+			yield return new WaitForSeconds(Random.Range(4, 7));
+		}
+	}
+}
